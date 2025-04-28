18d9684cddf43b4f24d93906f4a69ea1f764c32c
JohanAndreyGuarinReatiga <186079123+JohanAndreyGuarinReatiga@users.noreply.github.com>
2025-04-28 12:38:45 -0500

    DOM vars in js added

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-var variable = 0
+const elements = {
+    form: document.getElementById("form_creation"),
+    nameInput: document.getElementById("R-name"),
+    ingredientsInput: document.getElementById("Ingrdts"),
+    instructionsInput: document.getElementById("Instrcns"),
+    cookingInput: document.getElementById("Time"),
+    portionsInput: document.getElementById("Portions"),
+    hardnessSelect: document.getElementById("recipe-hardness"),
+    dishSelect: document.getElementById("recipe-dish"),
+    
+    form: document.getElementById("filters-section"),
+    nameSearch: document.getElementById("name-search"),
+    dishSearch: document.getElementById("recipe-select"),
+    categorySearch: document.getElementById("Category-select"),
+    
+}
